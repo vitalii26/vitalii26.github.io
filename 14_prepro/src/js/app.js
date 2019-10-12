d88b6ef38ec83a369b641c85e04af13f04f1c8e2
Yushchenko Vitalii <vitalik77795@ukr.net>
2019-10-13 00:48:49 +0300

    14_scss

--- 14_prepro/src/js/app.js
+++
@@ -1,9 +0,0 @@
-$('.sl').slick({
-	dots: true,
-	autoplay: true,
-	autoplaySpeed: 6000,
-	fade: true,
-	prevArrow: '<img class="slick-prev" src="../img/slick-prev.png">',
-	nextArrow: '<img class="slick-next" src="../img/slick-next.png">'
-
-});
---
+++ 14_prepros/src/js/app.js
@@ -0,0 +1,9 @@
+$('.sl').slick({
+	dots: true,
+	autoplay: true,
+	autoplaySpeed: 6000,
+	fade: true,
+	prevArrow: '<img class="slick-prev" src="./img/slick-prev.png">',
+	nextArrow: '<img class="slick-next" src="./img/slick-next.png">'
+
+});
